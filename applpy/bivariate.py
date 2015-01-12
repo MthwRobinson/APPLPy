9aca6d3096050ca94f422c0f51dec1818f479460
Matthew Robinson <mthw.wm.robinson@gmail.com>
2015-01-12 15:53:29 -0600

    display function added to bivariate rv class

--- applpy/bivariate.py
+++ applpy/bivariate.py
@@ -53,9 +53,9 @@ class BivariateRV:
 
     def __init__(self,func,constraints,ftype=['continuous','pdf']):
         """
-        Creates an instance of the bivariate random variable class
-
-        Data Structure:
+        Procedure Name: __init__
+        Purpose: Creates an instance of the bivariate random variable class
+        Arguments:
             self.func: a list of the functions f(x,y) for the random variable
             self.constraints: a list of constraints for the random variable.
                 The list of constraints must satisfy the following conditions:
@@ -68,10 +68,141 @@ class BivariateRV:
                     4. Except for constraints inthe form x<a or x>a, each
                         constraint must pass the vertical line test. i.e. There
                         should only be one y value associated with each x value.
+            self.ftype: a list of two strings. The first indicates whether the
+                random varable is discrete or continuous. The second specifies
+                the form for the represenation of the random variable (pdf,cdf,
+                etc)
+        Output:     1. An instance of the bivariate random variable class
         """
 
+        # If the function argument is not given in the form to a list, change
+        #   it into list format
 
+        if isinstance(func,list)!=True:
+            func1=func
+            func=[func1]
+        # Check to make sure that the constraints are given in the form of
+        #   a list
+        if isinstance(constraints,list)!=True:
+            raise RVError('Constraints must be entered as a list')
+        # Make sure that the random variable is either discrete or continuous
+        if ftype[0] not in ['continuous','discrete','Discrete']:
+            err_string='Random variables must be discrete or continuous'
+            raise RVError(err_string)
+        # Check to make sure the constraint list has the correct length
+        # The list of constraints should have the same number of elements
+        #   as the list of functions
+        if len(constraints)-len(func)!=0:
+            err_string='a bivariate random variable must have one set of'
+            err_string+=' constraints for each function that is entered'
+        
+        # Initialize the random variable
+        self.func=func
+        self.constraints=constraints
+        self.ftype=ftype
+        self.cache=None
 
+        """
+        Special Class Methods:
+
+        1. __repr__(self)
+        2. __len__(self)
+        """
+
+        def __repr__(self):
+            """
+            Procedure Name: __repr__
+            Purpose: Sets the default string display setting for the bivariate
+                        random variable class
+            Arguments:  1. self: the random variable
+            Output:     1. A series of print statements describing each
+                            segment of the random variable
+            """
+            return repr(self.display(opt='repr'))
+
+    def __len__(self):
+        """
+        Procedure Name: __len__
+        Purpose: Sets the behavior for the len() procedure when an instance
+                    of the random variable class is given as input. This
+                    procedure will return the number of pieces if the
+                    distribution is piecewise.
+        Arguments:  1. self: the random variable
+        Output:     1. the number of segments in the random variable
+        """
+        return len(self.func)
+
+        """
+        Utility Methods
+
+        Procedures:
+            1. add_to_cache(self,object_name,object)
+            2. display(self)
+            3. init_cache(self)
+        """
+
+    def add_to_cache(self,object_name,obj):
+        """
+        Procedure Name: add_to_cache
+        Purpose: Stores properties of the random variable (i.e. mean, variance,
+                    cdf, sf) in memory. The next time a function is called to
+                    compute that property, APPLPy will retrieve the object
+                    from memory.
+        Arguments:  1. self: the random variable
+                    2. object_name: the key for the object in the cache
+                        dictionary
+                    3. obj: the object to be stored in memory.
+        Output:     1. No output. The self.cache property of the random
+                        variable is modified to include the specified
+                        object.
+        """
+        # If a cache for the random variable does not exist, initialize it
+        if self.cache==None:
+            self.init_cache()
+        # Add an object to the cache dictionary
+        self.cache[object_name]=obj
+
+    def display(self,opt='repr'):
+        """
+        Procedure Name: display
+        Purpose: Displays the random variable in an interactive environment
+        Arugments:  1. self: the random variable
+        Output:     1. A print statement for each piece of the distribution
+                        indicating the function and the relevant support
+        """
+        if self.ftype[0] in ['continuous','Discrete']:
+            print ('%s %s'%(self.ftype[0],self.ftype[1]))
+            for i in range(len(self.func)):
+                cons_list=['0<'+str(cons) for cons in self.constraints[i]]
+                cons_string=', '.join(cons_list)
+                print('for x,y enclosed in the region:')
+                print(cons_string)
+                print('---------------------------')
+                pprint(self.func[i])
+                print('---------------------------')
+                if i<len(self.func)-1:
+                    print(' ');print(' ')
+            
+        if self.ftype[0]=='discrete':
+            print '%s %s where {x->f(x)}:'%(self.ftype[0],
+                                            self.ftype[1])
+            for i in range(len(self.support)):
+                if i!=(len(self.support)-1):
+                    print '{%s -> %s}, '%(self.support[i],
+                                          self.func[i]),
+                else:
+                    print '{%s -> %s}'%(self.support[i],
+                                        self.func[i])
+
+    def init_cache(self):
+        """
+        Procedure Name: init_cache
+        Purpose: Initializes the cache for the random variable
+        Arguments:  1. self: the random variable
+        Output:     1. The cache attribute for the random variable
+                            is initialized
+        """
+        self.cache={}
 
 
 
