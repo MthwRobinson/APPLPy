c35dbfcba31671b18fe0f9ab2b20c2190893028b
MthwRobinson <mthw.wm.robinson@gmail.com>
2015-05-25 23:10:31 -0500

    minor updates

--- applpy/dist_type.py
+++ applpy/dist_type.py
@@ -28,10 +28,11 @@ from __future__ import division
 from sympy import (Symbol, symbols, oo, integrate, summation, diff,
                    exp, pi, sqrt, factorial, ln, floor, simplify,
                    solve, nan, Add, Mul, Integer, function,
-                   binomial,gamma,cos)
+                   binomial,gamma,cos,cot)
 from random import random
 from .rv import (RV, RVError, CDF, CHF, HF, IDF, IDF, PDF, SF,
                  BootstrapRV, Convert)
+from .bivariate import (BivariateRV)
 x,y,z,t,v=symbols('x y z t v')
 
 """
@@ -1465,4 +1466,51 @@ class PoissonRV(RV):
         self.support=X_dummy.support
         self.ftype=X_dummy.ftype
         self.cache=None
+
+
+"""
+Bivariate Distributions
+"""
+
+class BivariateNormalRV(BivariateRV):
+    """
+    Procedure Name: BivariateNormalRV
+    Purpose: Creates an instance of the bivariate normal distribution
+    Arugments:  1. mu: a real valued parameter
+                2. sigma1: a strictly positive parameter
+                3. sigma2: a strictly positive parameter
+                4. rho: a parameter >=0 and <=1
+    Output:     1. A bivariate normal random variable
+    """
+    def __init__(self,mu=Symbol('mu'),sigma1=Symbol('sigma1',positive=True),
+                 sigma2=Symbol('sigma2',positive=True),rho=Symbol('rho')):
+        if rho.__class__.__name__!='Symbol':
+            if rho<0 or rho>1 :
+                err_string='rho must be >=0 and <=1'
+                raise RVError(err_string)
+        if sigma1.__class__.__name__!='Symbol':
+            if sigma1<=0:
+                err_string='sigma1 must be positive'
+                raise RVError(err_string)
+        if sigma2.__class__.__name__!='Symbol':
+            if sigma2<=0:
+                err_string='sigma2 must be positive'
+                raise RVError(err_string)
+
+        pdf_func=((1/(2*pi*sigma1*sigma2*sqrt(1-rho**2)))*
+                   exp(-mu/(2*(1-rho**2))))
+        X_dummy=BivariateRV([pdf_func],[[oo],[oo]],['continuous','pdf'])
+
+        self.func=X_dummy.func
+        self.constraints=X_dummy.constraints
+        self.ftype=X_dummy.ftype
+
+class ExampleRV(BivariateRV):
+    def __init__(self):
+        X_dummy=BivariateRV([(21/4)*x**2*y],[[1-y,y-sqrt(x)]]
+                                             ,['continuous','pdf'])
+        self.func=X_dummy.func
+        self.constraints=X_dummy.constraints
+        self.ftype=X_dummy.ftype
+
 
