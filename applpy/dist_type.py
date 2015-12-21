cc18df21481293e99d5984554dd3bd3cf55e3046
mrobinson72 <mrobinson72@gatech.edu>
2015-12-20 21:13:01 -0600

    BernoulliRV added

--- applpy/dist_type.py
+++ applpy/dist_type.py
@@ -1427,6 +1427,20 @@ class BinomialRV(RV):
         self.ftype=X_dummy.ftype
         self.cache={}
 
+class BernoulliRV(BinomialRV):
+    """
+    Procedure Name: BernoulliRV
+    Purpose: Creates an instance of the bernoulli distribution
+    Arguments:  1. p: a positive parameter between 0 and 1
+    Output:     1. A bernoulli random variable
+    """
+    def __init__(self,p=Symbol('p',positive=True)):
+        X_dummy = BinomialRV(N=1,p)
+        self.func=X_dummy.func
+        self.support=X_dummy.support
+        self.ftype=X_dummy.ftype
+        self.cache={}
+
 class GeometricRV(RV):
     """
     Procedure Name: GeometricRV
