d01b9be9f4bd0105ef1d2844b40922420ae17dff
Matt Robinson <mthw.wm.robinson@gmail.com>
2016-11-23 16:17:17 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 # To upload to PyPi run python setup.py sdist upload -r pypi
 setup(
     name='APPLPy',
-    version='0.4.10',
+    version='0.4.10'
     author='Matthew Robinson',
     author_email='mthw.wm.robinson@gmail.com',
     packages=['applpy','applpy.test'],
