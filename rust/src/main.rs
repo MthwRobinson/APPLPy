58315dc2a749190a715354b11180ca44868ff1cd
Matt Robinson <mthw.wm.robinson@gmail.com>
2026-03-07 22:24:19 -0500

    make sure you can pass in two

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,9 @@ mod order_stat;
 fn main() {
     let previous = [1, 2, 3, 4];
     let next = order_stat::next_combination(&previous, 2);
+    let another_next = order_stat::next_combination(&next, 2);
 
     println!("previous = {:?}", previous);
     println!("next = {:?}", next);
+    println!("next = {:?}", another_next);
 }
