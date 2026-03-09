1da930a0ed6b1ec084a2d60ad82098b09a699eda
Matt Robinson <mthw.wm.robinson@gmail.com>
2026-03-09 10:00:37 -0400

    initial implementation for next_combination

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ mod order_stat;
 fn main() {
     let previous = [1, 2, 3, 4];
     let next = order_stat::next_combination(&previous, 2);
-    let another_next = order_stat::next_combination(&next, 2);
+    let another_next = order_stat::next_combination(&previous, 3);
 
     println!("previous = {:?}", previous);
     println!("next = {:?}", next);
--- rust/src/order_stat.rs
+++ rust/src/order_stat.rs
@@ -1,9 +1,31 @@
 // Given the previous combination, finds the next lexicographical
 // combination
-pub fn next_combination(previous: &[usize], _n: usize) -> Vec<usize> {
-    let mut next = Vec::with_capacity(previous.len());
+pub fn next_combination(previous: &[usize], n: usize) -> Vec<usize> {
+    let num_elements: usize = previous.len();
+
+    let mut next = Vec::with_capacity(num_elements);
     next.extend_from_slice(previous);
 
-    next[0] = 99;
+    // If the value in the final position is not the maximum value it
+    // can attain, increment it by 1
+    if next[n-1] != n {
+        next[n-1] += 1;
+    } else {
+        let mut move_left: bool = true;
+        for i in 1..n {
+            let index = i - 1;
+            let upper_limit = n + i - n;
+            if next[index] < upper_limit {
+                for j in 1..upper_limit {
+                    next[index + j] = next[(index + j) - 1] + 1
+                }
+                move_left = false;
+            }
+            if !move_left {
+                break;
+            }
+        }
+    }
+
     next
 }
