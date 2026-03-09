e921384915bd0a323c56cd92702e4e437044cb0b
Matt Robinson <mthw.wm.robinson@gmail.com>
2026-03-09 15:02:26 -0400

    rv class passing clippy

--- rust/src/algorithms/rv.rs
+++ rust/src/algorithms/rv.rs
@@ -1,5 +1,7 @@
+#![allow(dead_code)]
+
 use num_rational::Rational64;
-use num_traits::ToPrimitive;
+use num_traits::cast::ToPrimitive;
 
 
 #[derive(Debug, Clone)]
@@ -33,36 +35,34 @@ pub struct RandomVariable {
 
 impl RandomVariable {
     fn verify_pdf(&self, tolerance: Option<f64>) -> Result<bool, String> {
-        let tolerance = tolerance.or_unwrap(1e-6);
+        let tolerance = tolerance.unwrap_or(0.00001);
 
 
         if self.ftype.0 != FunctionalForm::Pdf {
             return Err("verify_pdf only works for PDFs".to_string());
         }
 
-        let area: f64 = 0.0;
+        let mut area: f64 = 0.0;
         let support_len: usize = self.function.len();
 
         for i in 0..support_len {
 
-            let mut function: f64 = 0.0;
-            let function_value = self.function[i];
-            match function_value {
-                Number::Integer(x) => function = x as f64,
-                Number::Rational(x) => function = x.to_f64().unwrap(),
-            }
+            let function: f64 = match &self.function[i] {
+                Number::Float(x) => *x,
+                Number::Integer(x) => *x as f64,
+                Number::Rational(x) => x.to_f64().unwrap(),
+            };
 
-            let mut support: f64 = 0.0;
-            let support_value = self.support[i];
-            match support_value {
-                Number::Integer(x) => support = x as f64,
-                Number::Rational(x) => support = x.to_f64().unwrap(),
-            }
+            let support: f64 = match &self.support[i] {
+                Number::Float(x) => *x,
+                Number::Integer(x) => *x as f64,
+                Number::Rational(x) => x.to_f64().unwrap(),
+            };
 
             let probability = function * support;
             area += probability
         }
 
-        Ok((area > 1.0 - tolerance) & (area < 1.0 + tolerance))
+        Ok((area > 1.0 - tolerance) && (area < 1.0 + tolerance))
     }
 }
