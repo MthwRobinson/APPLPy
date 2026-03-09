c9312ac329066505e29af4150f39d414ee894f2e
Matt Robinson <mthw.wm.robinson@gmail.com>
2026-03-09 15:08:35 -0400

    more linting

--- rust/src/algorithms/rv.rs
+++ rust/src/algorithms/rv.rs
@@ -3,14 +3,12 @@
 use num_rational::Rational64;
 use num_traits::cast::ToPrimitive;
 
-
 #[derive(Debug, Clone)]
 pub enum Number {
     Float(f64),
     Integer(i64),
     Rational(Rational64),
 }
-
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum FunctionalForm {
@@ -32,11 +30,9 @@ pub struct RandomVariable {
     pub ftype: (FunctionalForm, DomainType),
 }
 
-
 impl RandomVariable {
     fn verify_pdf(&self, tolerance: Option<f64>) -> Result<bool, String> {
         let tolerance = tolerance.unwrap_or(0.00001);
-
 
         if self.ftype.0 != FunctionalForm::Pdf {
             return Err("verify_pdf only works for PDFs".to_string());
@@ -46,7 +42,6 @@ impl RandomVariable {
         let support_len: usize = self.function.len();
 
         for i in 0..support_len {
-
             let function: f64 = match &self.function[i] {
                 Number::Float(x) => *x,
                 Number::Integer(x) => *x as f64,
