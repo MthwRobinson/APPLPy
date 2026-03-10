f56b9cfeb142a3029042550c7e8ddab68537db45
Matt Robinson <mthw.wm.robinson@gmail.com>
2026-03-10 10:47:21 -0400

    fix main

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,9 +3,9 @@ mod algorithms;
 use algorithms::order_stat;
 
 fn main() {
-    let previous = [1, 2, 3, 4];
-    let next = order_stat::next_combination(&previous, 2).expect("No more combinations");
-    let another_next = order_stat::next_combination(&previous, 3).expect("No more combinations");
+    let previous = vec![0,1,2];
+    let next = order_stat::next_combination(&previous, 4).expect("No more combinations");
+    let another_next = order_stat::next_combination(&previous, 4).expect("No more combinations");
 
     println!("previous = {:?}", previous);
     println!("next = {:?}", next);
