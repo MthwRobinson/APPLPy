d0dc36921662ef21c45d840058692fc01e1e63e8
Matt Robinson <mthw.wm.robinson@gmail.com>
2026-03-27 14:53:27 -0400

    first pass on convolution

--- rust/src/algorithms/algebra.rs
+++ rust/src/algorithms/algebra.rs
@@ -129,6 +129,89 @@ pub fn product_discrete(
     Ok(product_rv)
 }
 
+/// Computes the product of two independent discrete random variables
+///
+/// # Arguments
+/// * `random_variable_1` - the first random variable
+/// * `random_variable_2` - the second random variable
+///
+/// # Returns
+/// * `product_rv` - the product of the two random variables
+///
+/// # Examples
+pub fn convolution_discrete(
+    random_variable_1: &RandomVariable,
+    random_variable_2: &RandomVariable,
+) -> Result<RandomVariable, String> {
+    let pdf_random_variable_1 = random_variable_1.to_pdf()?;
+    let function_1 = pdf_random_variable_1.function;
+    let support_1 = pdf_random_variable_1.support;
+
+    let pdf_random_variable_2 = random_variable_2.to_pdf()?;
+    let function_2 = pdf_random_variable_2.function;
+    let support_2 = pdf_random_variable_2.support;
+
+    // Find the values and probabilities for support_1 + support_2
+    // for all combinations of support values
+    let mut raw_conv_support = Vec::new();
+    for &s1 in support_1.iter() {
+        for &s2 in support_2.iter() {
+            let support_sum = s1 + s2;
+            raw_conv_support.push(support_sum);
+        }
+    }
+
+    let mut raw_conv_function = Vec::new();
+    for &f1 in function_1.iter() {
+        for &f2 in function_2.iter() {
+            let probability = f1 + f2;
+            raw_conv_function.push(probability);
+        }
+    }
+
+    // Sorts the results by the support values
+    let mut raw_conv_pairs: Vec<_> = raw_conv_support
+        .into_iter()
+        .zip(raw_conv_function)
+        .collect();
+
+    raw_conv_pairs.sort_by(|a, b| {
+        let first_value = a.0.to_f64();
+        let second_value = b.0.to_f64();
+        first_value.total_cmp(&second_value)
+    });
+
+    let (sorted_support, sorted_function): (Vec<Number>, Vec<Number>) =
+        raw_conv_pairs.into_iter().unzip();
+
+    // Remove redundant elements from the support
+    let mut conv_support = Vec::new();
+    let mut conv_function = Vec::new();
+
+    for (&s, &f) in sorted_support.iter().zip(sorted_function.iter()) {
+        let support_index = conv_support.iter().position(|&x| x == s);
+
+        match support_index {
+            Some(index) => {
+                conv_function[index] += f;
+            }
+            None => {
+                conv_support.push(s);
+                conv_function.push(f);
+            }
+        }
+    }
+
+    let conv_random_variable = RandomVariable {
+        function: conv_function,
+        support: conv_support,
+        functional_form: FunctionalForm::Pdf,
+        domain_type: DomainType::Discrete,
+    };
+
+    Ok(conv_random_variable)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
